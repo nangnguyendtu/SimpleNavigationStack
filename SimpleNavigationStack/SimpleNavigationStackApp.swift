0d071e60150520c52a5ad843b434e97b5b2a0418
Nang Nguyen <nangnguyendtu@gmail.com>
2024-09-24 20:38:09 +0700

    Initial Commit

---
+++ SimpleNavigationStack/SimpleNavigationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleNavigationStackApp.swift
+//  SimpleNavigationStack
+//
+//  Created by Mac on 24/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct SimpleNavigationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
